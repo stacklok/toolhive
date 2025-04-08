0aedbfbd76f77dc9c791d8d9831ee3533614b95a
ChrisJBurns <29541485+ChrisJBurns@users.noreply.github.com>
2025-04-08 14:49:11 +0100

    feat: removes mcp servers from client config if autodiscovery is enabled
    Signed-off-by: ChrisJBurns <29541485+ChrisJBurns@users.noreply.github.com>

--- pkg/client/editor.go
+++ pkg/client/editor.go
@@ -1,10 +1,15 @@
 package client
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/stacklok/toolhive/pkg/logger"
+)
 
 // ConfigEditor defines the interface for types which can edit MCP client config files.
 type ConfigEditor interface {
 	AddServer(config *ConfigFile, serverName, url string) error
+	RemoveServer(config *ConfigFile, serverName string) error
 }
 
 // StandardConfigEditor edits the MCP client config format used by most clients.
@@ -47,6 +52,34 @@ func (*StandardConfigEditor) AddServer(config *ConfigFile, serverName, url strin
 		mcpServersMap[serverName] = map[string]any{
 			"url": url,
 		}
+	}
+
+	return nil
+}
+
+// RemoveServer removes the specified MCP server from the Client config file.
+func (*StandardConfigEditor) RemoveServer(config *ConfigFile, serverName string) error {
+	// Get mcpServers object
+	mcpServers, ok := config.Contents["mcpServers"]
+	if !ok {
+		// Create mcpServers object if it doesn't exist
+		config.Contents["mcpServers"] = make(map[string]any)
+		mcpServers = config.Contents["mcpServers"]
+	}
+
+	// Convert to map
+	mcpServersMap, ok := mcpServers.(map[string]any)
+	if !ok {
+		return fmt.Errorf("mcpServers is not a map")
+	}
+
+	// Check if the server already exists
+	_, exists := mcpServersMap[serverName]
+	if exists {
+		delete(mcpServersMap, serverName)
+		logger.Log.Info(fmt.Sprintf("Removed MCP server %s from config file %s", serverName, config.Path))
+	} else {
+		logger.Log.Info(fmt.Sprintf("Nothing to do, MCP server %s was not found in config file %s", serverName, config.Path))
 	}
 
 	return nil
@@ -104,6 +137,44 @@ func (*VSCodeConfigEditor) AddServer(config *ConfigFile, serverName, url string)
 			"url":  url,
 			"type": "sse",
 		}
+	}
+
+	return nil
+}
+
+// RemoveServer removes the specified MCP server from the Client config file (VSCode)
+func (*VSCodeConfigEditor) RemoveServer(config *ConfigFile, serverName string) error {
+	// TODO: This pattern of "descend through JSON and apply a diff" can be generalized.
+	// Get mcp object
+	mcp, ok := config.Contents["mcp"]
+	if !ok {
+		// Create mcp object if it doesn't exist
+		config.Contents["mcp"] = make(map[string]any)
+		mcp = config.Contents["mcp"]
+	}
+	mcpMap := mcp.(map[string]any)
+
+	// Get servers child object
+	mcpServers, ok := mcpMap["servers"]
+	if !ok {
+		// Create servers object if it doesn't exist
+		(config.Contents["mcp"].(map[string]any))["servers"] = make(map[string]any)
+		mcpServers = (config.Contents["mcp"].(map[string]any))["servers"]
+	}
+
+	// Convert to map
+	mcpServersMap, ok := mcpServers.(map[string]any)
+	if !ok {
+		return fmt.Errorf("mcpServers is not a map")
+	}
+
+	// Check if the server already exists
+	_, exists := mcpServersMap[serverName]
+	if exists {
+		delete(mcpServersMap, serverName)
+		logger.Log.Info(fmt.Sprintf("Removed MCP server %s from config file %s", serverName, config.Path))
+	} else {
+		logger.Log.Info(fmt.Sprintf("Nothing to do, MCP server %s was not found in config file %s", serverName, config.Path))
 	}
 
 	return nil

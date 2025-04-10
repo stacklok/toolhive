0bf3ebbea2751ef433095c0069425af59222a477
Radoslav Dimitrov <radoslav@stacklok.com>
2025-04-10 13:28:18 +0300

    Fix the version command output while releasing (#159)
    Signed-off-by: Radoslav Dimitrov <radoslav@stacklok.com>

--- cmd/thv/main.go
+++ cmd/thv/main.go
@@ -4,7 +4,7 @@ package main
 import (
 	"os"
 
-	cli "github.com/StacklokLabs/toolhive/cmd/thv/app"
+	"github.com/StacklokLabs/toolhive/cmd/thv/app"
 	"github.com/StacklokLabs/toolhive/pkg/logger"
 )
 
@@ -12,7 +12,7 @@ func main() {
 	// Initialize the logger system
 	logger.Initialize()
 
-	if err := cli.NewRootCmd().Execute(); err != nil {
+	if err := app.NewRootCmd().Execute(); err != nil {
 		logger.Log.Error("%v, %v", os.Stderr, err)
 		os.Exit(1)
 	}

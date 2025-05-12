f4fc958aeaa3e713388d266bf61de3225d01f9ca
Juan Antonio Osorio <ozz@stacklok.com>
2025-05-12 16:28:49 +0300

    Initialize logger on command run (#369)
    Signed-off-by: Juan Antonio Osorio <ozz@stacklok.com>

--- cmd/thv/main.go
+++ cmd/thv/main.go
@@ -6,9 +6,13 @@ import (
 	"os"
 
 	"github.com/stacklok/toolhive/cmd/thv/app"
+	"github.com/stacklok/toolhive/pkg/logger"
 )
 
 func main() {
+	// Initialize the logger
+	logger.Initialize()
+
 	if err := app.NewRootCmd().Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "there was an error: %v\n", err)
 		os.Exit(1)

9eeeb693a9fce8d8417881cf8073b5416ff81d9d
Radoslav Dimitrov <radoslav@stacklok.com>
2025-06-05 14:09:45 +0300

    Initial implementation of the Registry API
    Signed-off-by: Radoslav Dimitrov <radoslav@stacklok.com>

--- pkg/api/v1/registry.go
+++ pkg/api/v1/registry.go
@@ -1,11 +1,18 @@
 package v1
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 
 	"github.com/stacklok/toolhive/pkg/logger"
+	"github.com/stacklok/toolhive/pkg/registry"
+)
+
+const (
+	// defaultRegistryName is the name of the default registry
+	defaultRegistryName = "default"
 )
 
 // RegistryRoutes defines the routes for the registry API.
@@ -37,8 +44,27 @@ func RegistryRouter() http.Handler {
 //		@Produce		json
 //		@Success		200	{object}	registryListResponse
 //		@Router			/api/v1beta/registry [get]
-func (*RegistryRoutes) listRegistries(_ http.ResponseWriter, _ *http.Request) {
-	logger.Debug("Listing registries")
+func (*RegistryRoutes) listRegistries(w http.ResponseWriter, _ *http.Request) {
+	reg, err := registry.GetRegistry()
+	if err != nil {
+		http.Error(w, "Failed to get registry", http.StatusInternalServerError)
+		return
+	}
+
+	registries := []registryInfo{
+		{
+			Name:        defaultRegistryName,
+			Version:     reg.Version,
+			LastUpdated: reg.LastUpdated,
+			ServerCount: len(reg.Servers),
+		},
+	}
+
+	response := registryListResponse{Registries: registries}
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+		return
+	}
 }
 
 //	 addRegistry
@@ -48,13 +74,12 @@ func (*RegistryRoutes) listRegistries(_ http.ResponseWriter, _ *http.Request) {
 //		@Tags			registry
 //		@Accept			json
 //		@Produce		json
-//		@Param			request	body		addRegistryRequest	true	"Add registry request"
-//		@Success		201		{object}	addRegistryResponse
-//		@Failure		400		{string}	string	"Bad Request"
-//		@Failure		409		{string}	string	"Conflict"
+//		@Success		501		{string}	string	"Not Implemented"
 //		@Router			/api/v1beta/registry [post]
-func (*RegistryRoutes) addRegistry(_ http.ResponseWriter, _ *http.Request) {
-	logger.Debug("Adding registry")
+func (*RegistryRoutes) addRegistry(w http.ResponseWriter, _ *http.Request) {
+	// Currently, only the default registry is supported
+	// This endpoint returns a 501 Not Implemented status
+	http.Error(w, "Adding custom registries is not currently supported", http.StatusNotImplemented)
 }
 
 //	 getRegistry
@@ -67,9 +92,34 @@ func (*RegistryRoutes) addRegistry(_ http.ResponseWriter, _ *http.Request) {
 //		@Success		200	{object}	getRegistryResponse
 //		@Failure		404	{string}	string	"Not Found"
 //		@Router			/api/v1beta/registry/{name} [get]
-func (*RegistryRoutes) getRegistry(_ http.ResponseWriter, r *http.Request) {
+func (*RegistryRoutes) getRegistry(w http.ResponseWriter, r *http.Request) {
 	name := chi.URLParam(r, "name")
-	logger.Debugf("Getting registry: %s", name)
+
+	// Only "default" registry is supported currently
+	if name != defaultRegistryName {
+		http.Error(w, "Registry not found", http.StatusNotFound)
+		return
+	}
+
+	reg, err := registry.GetRegistry()
+	if err != nil {
+		http.Error(w, "Failed to get registry", http.StatusInternalServerError)
+		return
+	}
+
+	response := getRegistryResponse{
+		Name:        defaultRegistryName,
+		Version:     reg.Version,
+		LastUpdated: reg.LastUpdated,
+		ServerCount: len(reg.Servers),
+		Registry:    reg,
+	}
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		logger.Errorf("Failed to encode response: %v", err)
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+		return
+	}
 }
 
 //	 removeRegistry
@@ -82,9 +132,17 @@ func (*RegistryRoutes) getRegistry(_ http.ResponseWriter, r *http.Request) {
 //		@Success		204	{string}	string	"No Content"
 //		@Failure		404	{string}	string	"Not Found"
 //		@Router			/api/v1beta/registry/{name} [delete]
-func (*RegistryRoutes) removeRegistry(_ http.ResponseWriter, r *http.Request) {
+func (*RegistryRoutes) removeRegistry(w http.ResponseWriter, r *http.Request) {
 	name := chi.URLParam(r, "name")
-	logger.Debugf("Removing registry: %s", name)
+
+	// Cannot remove the default registry
+	if name == defaultRegistryName {
+		http.Error(w, "Cannot remove the default registry", http.StatusBadRequest)
+		return
+	}
+
+	// Since only default registry exists, any other name is not found
+	http.Error(w, "Registry not found", http.StatusNotFound)
 }
 
 //	 listServers
@@ -97,9 +155,28 @@ func (*RegistryRoutes) removeRegistry(_ http.ResponseWriter, r *http.Request) {
 //		@Success		200	{object}	listServersResponse
 //		@Failure		404	{string}	string	"Not Found"
 //		@Router			/api/v1beta/registry/{name}/servers [get]
-func (*RegistryRoutes) listServers(_ http.ResponseWriter, r *http.Request) {
+func (*RegistryRoutes) listServers(w http.ResponseWriter, r *http.Request) {
 	registryName := chi.URLParam(r, "name")
-	logger.Debugf("Listing servers for registry: %s", registryName)
+
+	// Only "default" registry is supported currently
+	if registryName != defaultRegistryName {
+		http.Error(w, "Registry not found", http.StatusNotFound)
+		return
+	}
+
+	servers, err := registry.ListServers()
+	if err != nil {
+		logger.Errorf("Failed to list servers: %v", err)
+		http.Error(w, "Failed to list servers", http.StatusInternalServerError)
+		return
+	}
+
+	response := listServersResponse{Servers: servers}
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		logger.Errorf("Failed to encode response: %v", err)
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+		return
+	}
 }
 
 //	 getServer
@@ -113,8 +190,83 @@ func (*RegistryRoutes) listServers(_ http.ResponseWriter, r *http.Request) {
 //		@Success		200	{object}	getServerResponse
 //		@Failure		404	{string}	string	"Not Found"
 //		@Router			/api/v1beta/registry/{name}/servers/{serverName} [get]
-func (*RegistryRoutes) getServer(_ http.ResponseWriter, r *http.Request) {
+func (*RegistryRoutes) getServer(w http.ResponseWriter, r *http.Request) {
 	registryName := chi.URLParam(r, "name")
 	serverName := chi.URLParam(r, "serverName")
-	logger.Debugf("Getting server '%s' from registry '%s'", serverName, registryName)
+
+	// Only "default" registry is supported currently
+	if registryName != defaultRegistryName {
+		http.Error(w, "Registry not found", http.StatusNotFound)
+		return
+	}
+
+	server, err := registry.GetServer(serverName)
+	if err != nil {
+		logger.Errorf("Failed to get server '%s': %v", serverName, err)
+		http.Error(w, "Server not found", http.StatusNotFound)
+		return
+	}
+
+	response := getServerResponse{Server: server}
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		logger.Errorf("Failed to encode response: %v", err)
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+		return
+	}
+}
+
+// Response type definitions.
+
+// registryInfo represents basic information about a registry
+//
+//	@Description	Basic information about a registry
+type registryInfo struct {
+	// Name of the registry
+	Name string `json:"name"`
+	// Version of the registry schema
+	Version string `json:"version"`
+	// Last updated timestamp
+	LastUpdated string `json:"last_updated"`
+	// Number of servers in the registry
+	ServerCount int `json:"server_count"`
+}
+
+// registryListResponse represents the response for listing registries
+//
+//	@Description	Response containing a list of registries
+type registryListResponse struct {
+	// List of registries
+	Registries []registryInfo `json:"registries"`
+}
+
+// getRegistryResponse represents the response for getting a registry
+//
+//	@Description	Response containing registry details
+type getRegistryResponse struct {
+	// Name of the registry
+	Name string `json:"name"`
+	// Version of the registry schema
+	Version string `json:"version"`
+	// Last updated timestamp
+	LastUpdated string `json:"last_updated"`
+	// Number of servers in the registry
+	ServerCount int `json:"server_count"`
+	// Full registry data
+	Registry *registry.Registry `json:"registry"`
+}
+
+// listServersResponse represents the response for listing servers in a registry
+//
+//	@Description	Response containing a list of servers
+type listServersResponse struct {
+	// List of servers in the registry
+	Servers []*registry.Server `json:"servers"`
+}
+
+// getServerResponse represents the response for getting a server from a registry
+//
+//	@Description	Response containing server details
+type getServerResponse struct {
+	// Server details
+	Server *registry.Server `json:"server"`
 }

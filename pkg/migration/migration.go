765e3dab5a8b2e56277fed73f7db124d764c3346
Eleftheria Stein-Kousathana <eleftheria@stacklok.com>
2025-08-19 12:36:05 +0200

    Continuously check if group migration is needed (#1485)

--- pkg/migration/migration.go
+++ pkg/migration/migration.go
@@ -3,10 +3,8 @@ package migration
 
 import (
 	"context"
-	"fmt"
 	"sync"
 
-	"github.com/stacklok/toolhive/pkg/config"
 	"github.com/stacklok/toolhive/pkg/logger"
 )
 
@@ -17,13 +15,6 @@ var migrationOnce sync.Once
 // This is called once at application startup
 func CheckAndPerformDefaultGroupMigration() {
 	migrationOnce.Do(func() {
-		appConfig := config.GetConfig()
-
-		// Check if default group migration has already been performed
-		if appConfig.DefaultGroupMigration {
-			return
-		}
-
 		if err := performDefaultGroupMigration(); err != nil {
 			logger.Errorf("Failed to perform default group migration: %v", err)
 			return
@@ -33,9 +24,6 @@ func CheckAndPerformDefaultGroupMigration() {
 
 // performDefaultGroupMigration migrates all existing workloads to the default group
 func performDefaultGroupMigration() error {
-	fmt.Println("Migrating existing workloads to default group...")
-	fmt.Println()
-
 	migrator := &DefaultGroupMigrator{}
 	return migrator.Migrate(context.Background())
 }

235005b6a345a9057a16582ed3b981e3d19d4950
Bhuswarna Kashyap <bhuswarnakashyap@gmail.com>
2025-09-04 01:15:28 +0530

    remove Secret <name>:  prefix, add INFO logger

--- pkg/secrets/keyring/composite.go
+++ pkg/secrets/keyring/composite.go
@@ -6,6 +6,8 @@ package keyring
 import (
 	"fmt"
 	"runtime"
+
+	"github.com/stacklok/toolhive/pkg/logger"
 )
 
 const linuxOS = "linux"
@@ -66,6 +68,8 @@ func (*compositeProvider) logProviderSelection(providerName string) {
 			fmt.Printf("Using keyring provider: %s\n", providerName)
 		}
 	}()
+
+	logger.Info(fmt.Sprintf("Using keyring provider: %s", providerName))
 }
 
 func (c *compositeProvider) Set(service, key, value string) error {

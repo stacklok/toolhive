919cddb1238d2cbdabf588797a6f88f57bc18aa1
Don Browne <don@stacklok.com>
2025-09-09 17:38:49 +0100

    Use status files to retrieve PID
    This follows on from the previous PRs which migrated PIDs from the PID
    files to status files.

--- pkg/transport/proxy/manager.go
+++ pkg/transport/proxy/manager.go
@@ -9,34 +9,6 @@ import (
 // We may want to move these operations behind an interface. For now, they
 // have been moved to this package to keep proxy-related logic grouped together.
 
-// StopProcess stops the proxy process associated with the container
-func StopProcess(containerBaseName string) {
-	if containerBaseName == "" {
-		logger.Warnf("Warning: Could not find base container name in labels")
-		return
-	}
-
-	// Try to read the PID file and kill the process
-	pid, err := process.ReadPIDFile(containerBaseName)
-	if err != nil {
-		logger.Errorf("No PID file found for %s, proxy may not be running in detached mode", containerBaseName)
-		return
-	}
-
-	// PID file found, try to kill the process
-	logger.Infof("Stopping proxy process (PID: %d)...", pid)
-	if err := process.KillProcess(pid); err != nil {
-		logger.Warnf("Warning: Failed to kill proxy process: %v", err)
-	} else {
-		logger.Info("Proxy process stopped")
-	}
-
-	// Remove the PID file
-	if err := process.RemovePIDFile(containerBaseName); err != nil {
-		logger.Warnf("Warning: Failed to remove PID file: %v", err)
-	}
-}
-
 // IsRunning checks if the proxy process is running
 func IsRunning(containerBaseName string) bool {
 	logger.Debugf("Checking if proxy process is running for container %s", containerBaseName)

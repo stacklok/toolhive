f8c15cc49aa9c5dd8851eba69cf36b377bc2f5c7
Daniele Martinoli <dmartino@redhat.com>
2025-09-15 17:12:23 +0200

    review comments
    Signed-off-by: Daniele Martinoli <dmartino@redhat.com>

--- cmd/thv-operator/pkg/sources/storage_manager.go
+++ cmd/thv-operator/pkg/sources/storage_manager.go
@@ -18,13 +18,6 @@ import (
 )
 
 const (
-// ConfigMapStorageDataKey is the key used to store registry data in ConfigMaps by the storage manager
-// ConfigMapStorageDataKey = "registry.json"
-// RegistryStorageComponent is the component label for the registry storage
-// RegistryStorageComponent = "registry-storage"
-)
-
-const (
 	// ConfigMapStorageDataKey is the key used to store registry data in ConfigMaps by the storage manager
 	ConfigMapStorageDataKey = "registry.json"
 	// RegistryStorageComponent is the component label for the registry storage

06d815edcc30f70024147d1b43c2bc8e335df8b3
Don Browne <don@stacklok.com>
2025-09-17 16:28:58 +0100

    ok linter

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -242,7 +242,7 @@ func (c *Config) saveToPath(configPath string) error {
 		return fmt.Errorf("error writing config file: %w", err)
 	}
 
-	// Important: Need to reset config singleton or else it wont pick up changes.
+	// Important: Need to reset config singleton or else it won't pick up changes.
 	ResetSingleton()
 
 	return nil

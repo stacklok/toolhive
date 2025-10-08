258cc25e58d2e51b96976d30ecd6b7541796a0de
Abhishek <abhishek.work71@gmail.com>
2025-10-08 23:07:44 +0530

    cleanup moved files

--- test/e2e/operator/doc.go
+++
@@ -1,3 +0,0 @@
-// Package operator_test provides end-to-end tests for the ToolHive operator controllers.
-// This package tests MCPRegistry and other operator functionality using Ginkgo and Kubernetes APIs.
-package operator_test

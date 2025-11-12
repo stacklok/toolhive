99035e053fbd77c50a940f832419eef4b4fe70b5
Dan Barr <danbarr@users.noreply.github.com>
2025-11-12 05:20:30 -0500

    Sort clients in the client setup command (#2551)
    Signed-off-by: Dan Barr <6922515+danbarr@users.noreply.github.com>
    Co-authored-by: Dan Barr <6922515+danbarr@users.noreply.github.com>

--- cmd/thv/app/client.go
+++ cmd/thv/app/client.go
@@ -3,6 +3,7 @@ package app
 import (
 	"context"
 	"fmt"
+	"sort"
 
 	"github.com/spf13/cobra"
 
@@ -136,6 +137,11 @@ func clientSetupCmdFunc(cmd *cobra.Command, _ []string) error {
 		fmt.Println("No new clients found.")
 		return nil
 	}
+
+	// Sort clients alphabetically by ClientType
+	sort.Slice(availableClients, func(i, j int) bool {
+		return availableClients[i].ClientType < availableClients[j].ClientType
+	})
 	// Get available groups for the UI
 	groupManager, err := groups.NewManager()
 	if err != nil {

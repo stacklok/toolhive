7555cccc3af6c7371aaaeee6d2a2c8f61b19abd0
Chris Burns <29541485+ChrisJBurns@users.noreply.github.com>
2025-11-19 18:55:51 +0000

    updates tests
    Signed-off-by: Chris Burns <29541485+ChrisJBurns@users.noreply.github.com>

--- cmd/thv-operator/pkg/registryapi/deployment_test.go
+++ cmd/thv-operator/pkg/registryapi/deployment_test.go
@@ -145,14 +145,14 @@ func TestGetRegistryAPIImage(t *testing.T) {
 		{
 			name:        "default image when env not set",
 			setEnv:      false,
-			expected:    "ghcr.io/stacklok/thv-registry-api:v0.1.0",
+			expected:    "ghcr.io/stacklok/thv-registry-api:latest",
 			description: "Should return default image when environment variable is not set",
 		},
 		{
 			name:        "default image when env empty",
 			envValue:    "",
 			setEnv:      true,
-			expected:    "ghcr.io/stacklok/thv-registry-api:v0.1.0",
+			expected:    "ghcr.io/stacklok/thv-registry-api:latest",
 			description: "Should return default image when environment variable is empty",
 		},
 		{

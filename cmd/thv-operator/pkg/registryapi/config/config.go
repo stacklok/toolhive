29022d7542139db031cc05d29c11cd743fcb11d0
Juan Antonio Osorio <ozz@stacklok.com>
2025-12-03 06:20:04 -0600

    Use gopkg.in/yaml.v3 not v2
    A change slipped using the older version. This fixes that.
    
    Signed-off-by: Juan Antonio Osorio <ozz@stacklok.com>

--- cmd/thv-operator/pkg/registryapi/config/config.go
+++ cmd/thv-operator/pkg/registryapi/config/config.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"path/filepath"
 
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"

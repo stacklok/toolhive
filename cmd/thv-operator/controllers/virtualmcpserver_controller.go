9ccabd83e83ae2b294dd09185d26b4db93c4695f
Chris Burns <29541485+ChrisJBurns@users.noreply.github.com>
2025-12-05 18:58:41 +0000

    removes duplicate var block
    Signed-off-by: Chris Burns <29541485+ChrisJBurns@users.noreply.github.com>

--- cmd/thv-operator/controllers/virtualmcpserver_controller.go
+++ cmd/thv-operator/controllers/virtualmcpserver_controller.go
@@ -85,9 +85,6 @@ var (
 // +kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=create;delete;get;list;patch;update;watch
 // +kubebuilder:rbac:groups="",resources=serviceaccounts,verbs=create;delete;get;list;patch;update;watch
-var (
-	envVarSanitizeRegex = regexp.MustCompile(`[^A-Z0-9_]`)
-)
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.

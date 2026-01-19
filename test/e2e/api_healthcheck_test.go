b39589d8a47db31b77539c21573047b29a7e6095
Don Browne <don@stacklok.com>
2026-01-19 12:45:58 +0000

    add e2e label

--- test/e2e/api_healthcheck_test.go
+++ test/e2e/api_healthcheck_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stacklok/toolhive/test/e2e"
 )
 
-var _ = Describe("Healthcheck API", Label("api", "healthcheck"), func() {
+var _ = Describe("Healthcheck API", Label("api", "healthcheck", "e2e"), func() {
 	var (
 		config    *e2e.ServerConfig
 		apiServer *e2e.Server

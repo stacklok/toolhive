9c8ec1b162ffa09660643d58f37fc1ff9ff1f77b
nigel brown <nigel@stacklok.com>
2026-01-21 16:23:27 +0000

    Add license headers to all optimizer package files

--- pkg/optimizer/db/sqlite_fts.go
+++ pkg/optimizer/db/sqlite_fts.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright 2025 Stacklok, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 // Package db provides database operations for the optimizer.
 // This file handles FTS5 (Full-Text Search) using modernc.org/sqlite (pure Go).
 package db

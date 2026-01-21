1c5e5af8893154db2fa9cbef06448737167ea5f1
nigel brown <nigel@stacklok.com>
2026-01-21 16:33:55 +0000

    Add license headers to remaining files missing SPDX headers

--- cmd/vmcp/app/commands.go
+++ cmd/vmcp/app/commands.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright 2025 Stacklok, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 // Package app provides the entry point for the vmcp command-line application.
 package app
 

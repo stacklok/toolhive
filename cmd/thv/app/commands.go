a47acea210bb4ce3977a41f1b2327f1e4d30dbc1
Samuele Verzi <samu@stacklok.com>
2026-02-18 14:57:54 +0100

    Suppress update message if is trigger by desktop

--- cmd/thv/app/commands.go
+++ cmd/thv/app/commands.go
@@ -116,6 +116,10 @@ func checkForUpdates() {
 	if updates.ShouldSkipUpdateChecks() {
 		return
 	}
+	// Desktop app manages its own updates
+	if desktop.IsDesktopManagedCLI() {
+		return
+	}
 
 	versionClient := updates.NewVersionClient()
 	updateChecker, err := updates.NewUpdateChecker(versionClient)
